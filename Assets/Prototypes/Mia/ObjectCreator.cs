f16e6824d677249fbefa55ab858aa92330913412
Mia <email@email.dk>
2016-11-01 19:34:54 +0100

    Mia prototype

---
+++ Assets/Prototypes/Mia/ObjectCreator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ObjectCreator : Editor {
+
+	[MenuItem ("GameObject/Create Simple Prefab")]
+	static void DoCreateSimplePrefab()
+	{
+		Object prefab = EditorUtility.CreateEmptyPrefab ("Assets/Prototypes/Mia/" + "genericName" + ".prefab");
+		//EditorUtility.ReplacePrefab (t.gameObject, prefab, ReplacePrefabOptions.ConnectToPrefab);
+	}
+
+	/*public override void OnInspectorGUI()
+	{
+		LevelScript myTarget = (LevelScript)target;
+
+		myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience);
+		EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
+	}*/
+}

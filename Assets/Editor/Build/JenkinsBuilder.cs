ac9e25580f40edf9425903e84f73304aaad64746
Bjørn <email@email.dk>
2016-11-02 17:56:12 +0100

    Build script for jenkins

---
+++ Assets/Editor/Build/JenkinsBuilder.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+public class JenkinsBuilder : MonoBehaviour
+{
+    private const string BUILD_FOLDER = "Builds";
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    private static void BuildAlpha()
+    {
+        Build("SpacedOut_Alpha");
+    }
+
+    private static void BuildBeta()
+    {
+        Build("SpacedOut_Beta");
+    }
+
+    private static void BuildMain()
+    {
+        Build("SpacedOut_Main");
+    }
+
+    private static void BuildFinal()
+    {
+        Build("SpacedOut");
+    }
+
+    private static void Build(string name)
+    {
+        // load the scenes
+        var scenes = FindEnabledEditorScenes();
+
+        if (scenes == null || scenes.Length == 0)
+        {
+            throw new UnityException("No scenes to build.");
+        }
+
+        Directory.CreateDirectory(BUILD_FOLDER);
+
+        UnityException exc = null;
+
+        try
+        {
+            BuildPipeline.BuildPlayer(
+                scenes,
+                BUILD_FOLDER + "/" + name + ".apk",
+                BuildTarget.Android, BuildOptions.None);
+        }
+        catch (UnityException e)
+        {
+            exc = e;
+        }
+
+        if (exc != null)
+            throw exc;
+    }
+}

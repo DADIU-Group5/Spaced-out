7c431c59de0e070ec636317b8a48eec9f5dfc46b
Camilla <camilla.g.jakobsen@hotmail.com>
2016-11-02 16:15:35 +0100

    first ragdoll test, with imported test asset

---
+++ Assets/Prototypes/Ragdoll/RagdollController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RagdollController : MonoBehaviour {
+
+    public Rigidbody[] ragdollRig;
+    public Rigidbody dave;
+
+    // Use this for initialization
+    void Start()
+    {
+        //find ragdoll joints in ragdollrigged gameobject, acess their rigidbodies
+        ragdollRig = this.GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody joint in ragdollRig)
+        {
+            //isKinematic
+            joint.isKinematic = true;
+        }
+        //dave.isKinematic = false;
+        //dave.useGravity = false;
+    }
+
+    public void RagdollStart()
+    {
+        //start ragdolling
+        foreach (Rigidbody joint in ragdollRig)
+        {
+            //isKinematic
+            joint.isKinematic = false;
+        }
+        //dave.isKinematic = false;
+        //dave.useGravity = false;
+        //disable animator here
+    }
+
+    public void RagdollStop()
+    {
+        //stop ragdolling
+        foreach (Rigidbody joint in ragdollRig)
+        {
+            //isKinematic
+            joint.isKinematic = true;
+        }
+        //dave.isKinematic = false;
+        //dave.useGravity = false;
+        //enable animator here
+    }
+}

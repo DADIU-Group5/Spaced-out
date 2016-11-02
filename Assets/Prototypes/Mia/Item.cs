9ee61d29cbdb5f29d35dc70f9f08b1a8b19545a5
Mia <email@email.dk>
2016-11-02 09:53:08 +0100

    Added basic Item class

---
+++ Assets/Prototypes/Mia/Item.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//anything else?
+public enum Movement
+{
+    none, staticItem, floatingItem
+}
+
+//just added some for example.
+public enum Behaviour
+{
+    none, sticky, bouncy, slippery, fire,
+    ice, grabby
+}
+
+public class Item : MonoBehaviour {
+
+    
+
+    public Behaviour behaviour = Behaviour.none;
+    public Movement movement = Movement.none;
+
+    public void SetBehaviour(Behaviour _behaviour)
+    {
+        behaviour = _behaviour;
+    }
+
+    public void SetMovement(Movement _movement)
+    {
+        movement = _movement;
+    }
+
+}

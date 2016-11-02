c773aa124f5de3b492e3ce5fdf4f17b972152c2f
Bjørn <email@email.dk>
2016-11-02 13:49:42 +0100

    Structure and simple navigation done

---
+++ Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuManager : Singleton<MainMenuManager> {
+
+    private Button startBtn;
+    private LevelSelectManager levelSelect;
+    private Button settingsBtn;
+    private Button shopBtn;
+    private Button exitBtn;
+    // The active view
+    private GameObject view;
+
+    public void OnStartBtnClick()
+    {
+
+    }
+
+    public void OnLevelSelectBtnClick()
+    {
+        gameObject.SetActive(false);
+        levelSelect.gameObject.SetActive(true);
+        view = levelSelect.gameObject;
+    }
+
+    public void OnSettingsBtnClick()
+    {
+
+    }
+
+    public void OnShopBtnClick()
+    {
+
+    }
+
+    public void Back()
+    {
+        view.SetActive(false);
+    }
+}

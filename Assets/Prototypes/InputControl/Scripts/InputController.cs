df5cdeaa46af132c7ddfa213c7dda8132a767758
Nicholas Indrehus <nicholas.indrehus@gmail.com>
2016-11-02 13:32:32 +0100

    Created simple prototype for testing controls. Currently only supports hold and release.

---
+++ Assets/Prototypes/InputControl/Scripts/InputController.cs
@@ -0,0 +1,280 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class InputController : MonoBehaviour
+{
+    private bool primed = false;
+    private Vector2 oldPoint;
+    private Camera current;
+    private Plane inputPlane;
+
+    public bool tilt = false, sidescroll = false, swipeRotate = true;
+    public float cameraRotateSpeed = 4000f;
+    public Camera behind;
+    public BehindCamera behindCamera;
+    public PlayerController player;
+    public Transform playerTransform, playerPitchTransform;
+
+    public ControlMode selectedControl = ControlMode.HoldButton;
+
+    public List<string> controlNames;
+
+    public Text controlText;
+
+    private void Awake()
+    {
+        inputPlane = new Plane(Vector3.right, Vector3.zero);
+
+        controlNames = new List<string>(){
+            "Hold - Button",
+            "Hold - Fingers",
+            "Sling - Two Fingers",
+            "Cross",
+            "Swipe",
+            "Jetpack micro corrections"
+        };
+
+        controlText.text = controlNames[(int)selectedControl];
+    }
+
+    private void ResetRotation()
+    {
+        playerTransform.rotation = Quaternion.identity;
+        playerPitchTransform.rotation = Quaternion.identity;
+    }
+
+    private Vector2 ScreenCenter()
+    {
+        return new Vector2(current.pixelWidth / 2f, current.pixelHeight / 2f);
+    }
+
+    public enum ControlMode
+    {
+        HoldButton,
+        HoldFingers,
+        SlingTheMofo,
+        Cross,
+        Swipe,
+        JetPack
+    }
+
+    public void ChangeControlLeft()
+    {
+        if ((int)selectedControl <= 0)
+            selectedControl = ControlMode.JetPack;
+        else
+            selectedControl--;
+
+        controlText.text = controlNames[(int)selectedControl];
+    }
+
+    public void ChangeControlRight()
+    {
+        if ((int)selectedControl >= 4)
+            selectedControl = ControlMode.HoldButton;
+        else
+            selectedControl++;
+
+        controlText.text = controlNames[(int)selectedControl];
+    }
+
+    private void Update()
+    {
+        // Quit
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        switch (selectedControl)
+        {
+            case ControlMode.HoldButton:
+                current = behind;
+                behind.enabled = true;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    oldPoint = Input.mousePosition;
+                }
+
+                // Look
+                if (Input.GetMouseButton(0))
+                {
+                    Vector2 pos = Input.mousePosition;
+                    Vector2 offset;
+                    if (swipeRotate)
+                    {
+                        offset = pos - oldPoint;
+                    }
+                    else
+                    {
+                        offset = pos - ScreenCenter();
+                    }
+                    DirectedRotation(offset, tilt);
+                    oldPoint = pos;
+                }
+
+                break;
+
+            case ControlMode.HoldFingers:
+                current = behind;
+                behind.enabled = true;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    oldPoint = Input.mousePosition;
+                }
+
+                // Look
+                if (Input.GetMouseButton(0))
+                {
+                    Vector2 pos = Input.mousePosition;
+                    Vector2 offset;
+                    if (swipeRotate)
+                    {
+                        offset = pos - oldPoint;
+                    }
+                    else
+                    {
+                        offset = pos - ScreenCenter();
+                    }
+                    DirectedRotation(offset, tilt);
+                    oldPoint = pos;
+                }
+
+                // Rotate player so it faces camera direction
+                if (Input.GetMouseButton(1))
+                {
+                    player.GetComponent<Rigidbody>().freezeRotation = true;
+                    playerTransform.rotation = behindCamera.transform.rotation;
+                    playerPitchTransform.rotation = behindCamera.pitch.transform.rotation;
+                }
+
+                // Move
+                if (Input.GetMouseButtonDown(1))
+                {
+                    player.SetCharging(true);
+                }
+
+                // Launch
+                if (Input.GetMouseButtonUp(1))
+                {
+                    player.GetComponent<Rigidbody>().freezeRotation = false;
+                    player.LaunchCharge(behindCamera.pitch.transform.forward);
+                    player.SetCharging(false);
+                }
+
+                break;
+            case ControlMode.SlingTheMofo:
+                current = behind;
+                behind.enabled = true;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    oldPoint = Input.mousePosition;
+                }
+
+                // Look
+                if (Input.GetMouseButton(0))
+                {
+                    Vector2 pos = Input.mousePosition;
+                    Vector2 offset;
+                    if (swipeRotate)
+                    {
+                        offset = pos - oldPoint;
+                    }
+                    else
+                    {
+                        offset = pos - ScreenCenter();
+                    }
+                    DirectedRotation(offset, tilt);
+                    oldPoint = pos;
+                }
+
+                // Rotate player so it faces camera direction
+                if (Input.GetMouseButton(1))
+                {
+                    player.GetComponent<Rigidbody>().freezeRotation = true;
+                    playerTransform.rotation = behindCamera.transform.rotation;
+                    playerPitchTransform.rotation = behindCamera.pitch.transform.rotation;
+                }
+
+                // Move
+                if (Input.GetMouseButtonDown(1))
+                {
+                    player.SetCharging(true);
+                }
+
+                // Launch
+                if (Input.GetMouseButtonUp(1))
+                {
+                    player.GetComponent<Rigidbody>().freezeRotation = false;
+                    player.LaunchCharge(behindCamera.pitch.transform.forward);
+                    player.SetCharging(false);
+                }
+                break;
+            case ControlMode.Cross:
+                break;
+            case ControlMode.Swipe:
+                break;
+            case ControlMode.JetPack:
+                break;
+        }
+
+    }
+
+
+    public void OnChargeButtonPressed()
+    {
+        // Rotate player so it faces camera direction
+        player.GetComponent<Rigidbody>().freezeRotation = true;
+        playerTransform.rotation = behindCamera.transform.rotation;
+        playerPitchTransform.rotation = behindCamera.pitch.transform.rotation;
+
+        // Move
+        player.SetCharging(true);
+    }
+
+    public void OnChargeButtonHeld()
+    {
+
+    }
+
+    public void OnChargeButtonReleased()
+    {
+        // Launch
+        player.GetComponent<Rigidbody>().freezeRotation = false;
+        player.LaunchCharge(behindCamera.pitch.transform.forward);
+        player.SetCharging(false);
+    }
+
+    public void DirectedRotation(Vector2 offset, bool tilt)
+    {
+        /*
+        if (tilt)
+        {
+            Vector3 axis = new Vector3(-offset.y, offset.x, 0);
+            playerTransform.Rotate(axis, Time.deltaTime * cameraRotateSpeed * (offset.magnitude / ScreenCenter().magnitude));
+        }
+        else
+        {
+            float xScale = playerPitchTransform.up.y;
+            playerTransform.Rotate(Vector3.up, Time.deltaTime * xScale * cameraRotateSpeed * (offset.x / ScreenCenter().magnitude));
+            playerPitchTransform.Rotate(Vector3.right, Time.deltaTime * cameraRotateSpeed * (-offset.y / ScreenCenter().magnitude));
+        }
+        */
+        if (tilt)
+        {
+            Vector3 axis = new Vector3(-offset.y, offset.x, 0);
+            behindCamera.transform.Rotate(axis, Time.deltaTime * cameraRotateSpeed * (offset.magnitude / ScreenCenter().magnitude));
+        }
+        else
+        {
+            float xScale = behindCamera.pitch.transform.up.y;
+            behindCamera.transform.Rotate(Vector3.up, Time.deltaTime * xScale * cameraRotateSpeed * (offset.x / ScreenCenter().magnitude));
+            behindCamera.pitch.transform.Rotate(Vector3.right, Time.deltaTime * cameraRotateSpeed * (-offset.y / ScreenCenter().magnitude));
+        }
+    }
+}
---
+++ Assets/Prototypes/InputControl/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool charging = false, increasing = false;
+    private float launchForce = 0f;
+
+    public float minLaunchForce = 100f, maxLaunchForce = 200f, launchSideScale = 10f, timeToMax = 1f;
+    public Transform pitchTransform;
+    public Text chargeText;
+    public Transform chargeArrow;
+    float chargeArrowYMin = -700.0f;
+    float chargeArrowYHeight = 400.0f;
+
+    private void Update()
+    {
+        if (charging)
+        {
+            Charge();
+        }
+        else
+        {
+            launchForce = 0;
+        }
+        chargeText.text = "" + launchForce;
+
+        chargeArrow.transform.position = new Vector3(chargeArrow.position.x, chargeArrowYMin + chargeArrowYHeight * launchForce / maxLaunchForce);
+    }
+
+    private void Charge()
+    {
+        float delta = (Time.deltaTime * (maxLaunchForce - minLaunchForce)) / timeToMax;
+        if (increasing)
+        {
+            launchForce += delta;
+            if (launchForce > maxLaunchForce)
+            {
+                launchForce = 2 * maxLaunchForce - launchForce;
+                increasing = false;
+            }
+        }
+        else
+        {
+            launchForce -= delta;
+            if (launchForce < minLaunchForce)
+            {
+                launchForce = 2 * minLaunchForce - launchForce;
+                increasing = true;
+            }
+        }
+    }
+
+    public void SetCharging(bool value)
+    {
+        charging = value;
+        if (charging)
+        {
+            launchForce = minLaunchForce;
+            increasing = true;
+        }
+        else
+        {
+            launchForce = 0f;
+        }
+    }
+
+    public void LaunchCharge()
+    {
+        LaunchCharge(pitchTransform.forward);
+    }
+
+    public void LaunchCharge(Vector3 direction)
+    {
+        Launch(direction, launchForce);
+    }
+
+    public void LaunchScale(Vector3 distance)
+    {
+        Launch(distance, distance.magnitude * launchSideScale);
+    }
+
+    public void Launch(Vector3 direction, float force)
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddForce(force * direction.normalized);
+    }
+}

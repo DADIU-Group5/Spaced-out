cc72707d03996acd8b9977af77657c887049adad
Mia <email@email.dk>
2016-11-07 16:19:35 +0100

    Fire and electricity now kills.

--- Assets/Scripts/Gameplay Elements/ElectricalHazard.cs
+++ Assets/Scripts/Gameplay Elements/ElectricalHazard.cs
@@ -1,39 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FireHazard : MonoBehaviour {
+public class ElectricalHazard : MonoBehaviour {
 
-    public float TimeUntilBurnToDeath = 20f;
+    public float TimeUntilShockToDeath = 0f;
 
     [HideInInspector]
     public GameObject player;
-    private bool burningPlayer = false;
+    private bool shockingPlayer = false;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         player = GameObject.FindGameObjectWithTag("Player");
-	}
+    }
 
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag == "Player" && !burningPlayer)
+        if (other.transform.tag == "Player" && !shockingPlayer)
         {
-            burningPlayer = true;
+            shockingPlayer = true;
             player.GetComponent<PlayerController>().onFire = true;
-            StartCoroutine(BurnToDeath());
+            StartCoroutine(ShockToDeath());
         }
     }
 
     /// <summary>
     /// Burn the player to death, if fire is not put out.
     /// </summary>
-    public IEnumerator BurnToDeath()
+    public IEnumerator ShockToDeath()
     {
-        yield return new WaitForSeconds(TimeUntilBurnToDeath);
+        yield return new WaitForSeconds(TimeUntilShockToDeath);
 
         //if the player is still on fire after this time, die.
-        if (player.GetComponent < PlayerController >(). onFire)
+        if (player.GetComponent<PlayerController>().onFire)
         {
             Debug.Log("Player has burned to death!");
             player.GetComponent<PlayerController>().Kill();

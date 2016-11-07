4599e6058bc86a88d65d6dbd06d6fa70bca15552
Victor <Victor@Victors-MacBook-Pro.local>
2016-11-07 16:05:40 +0100

    no files over 100 mb. switch group for different gal voice versions

---
+++ Wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,85 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID DAVECHARGE = 2392150155U;
+        static const AkUniqueID DAVEFIRSTLAUNCH = 1537918958U;
+        static const AkUniqueID DAVEFIRSTLAUNCH_01 = 2606874748U;
+        static const AkUniqueID DAVEFIRSTLAUNCH_02 = 2606874751U;
+        static const AkUniqueID DAVEFIRSTLAUNCH_03 = 2606874750U;
+        static const AkUniqueID DAVEFIRSTLAUNCH_04 = 2606874745U;
+        static const AkUniqueID DAVEFIRSTLAUNCH_05 = 2606874744U;
+        static const AkUniqueID DAVELAUNCH = 3193570530U;
+        static const AkUniqueID EXPLOSIVE = 433216386U;
+        static const AkUniqueID GALDAVEONFIRE = 181220522U;
+        static const AkUniqueID GALDEATHELECTROCUTED = 3469266988U;
+        static const AkUniqueID GALHAZARDSEXPLOSION = 1952453951U;
+        static const AkUniqueID GALRANDOMINSULT = 1842267159U;
+        static const AkUniqueID GALRANDOMNARATIVE = 2136770720U;
+        static const AkUniqueID GAMESTART = 4058101365U;
+        static const AkUniqueID MUSICPLAY = 1205645828U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GALVERSION
+        {
+            static const AkUniqueID GROUP = 3851149549U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID V1 = 1534528634U;
+                static const AkUniqueID V2 = 1534528633U;
+                static const AkUniqueID V3 = 1534528632U;
+            } // namespace SWITCH
+        } // namespace GALVERSION
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID JETPACKCHARGELEVEL = 2371628509U;
+        static const AkUniqueID MASTERHIPASS = 3129080009U;
+        static const AkUniqueID MASTERLOPASS = 3097485287U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK_ALPHA = 1770303713U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID ATMOS = 3034623661U;
+        static const AkUniqueID GAL = 579662023U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__

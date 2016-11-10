3a7ca543dc1ffc5cd87052753713d961d817757a
Frederik <fknudsen@gmail.com>
2016-11-10 13:59:04 +0100

    Made some changes to the room editor. Updated the player, to have a scale of 1,1,1 and added the behind camera scrip to it. IF there are conflicts, you can discard these.

---
+++ Assets/Scripts/World/Room Editor/PickupSelector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupSelector : ObjectSelector {
+
+    public override void LoadObjects()
+    {
+        canBe = ObjectDatabase.instance.GetPickupObjects();
+        base.LoadObjects();
+    }
+}

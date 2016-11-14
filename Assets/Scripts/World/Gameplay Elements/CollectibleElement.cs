6db501af4b6ad94b445b5247f255ee646a27b88c
Frederik <fknudsen@gmail.com>
2016-11-14 11:30:30 +0100

    Made collectibles count up and be able to unlock achievement for that level. Made the player save the amount of fuel he has, when dying before getting to the first checkpoint.

--- Assets/Scripts/World/Gameplay Elements/CollectibleElement.cs
+++ Assets/Scripts/World/Gameplay Elements/CollectibleElement.cs
@@ -3,35 +3,17 @@ using System.Collections;
 
 public class CollectibleElement : MonoBehaviour {
 
-    [HideInInspector]
-    public ScoreManager _scoreManager;
-
-    private int level = 0;
-
-    // Use this for initialization
-    void Start () {
-        _scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
-        level = PlayerPrefs.GetInt("CurrentLevel");
+    void Start()
+    {
+        ScoreManager.instance.AddCollectibleToLevel();
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
     void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
         {
-            if (_scoreManager != null)
-            {
-                _scoreManager.AddCollectibles(level);
-            }
-            else
-            {
-                Debug.Log("CollectibleElement couldnt find the scoremanager.");
-            }
-            Destroy(this);
+            ScoreManager.instance.AddCollectibles();
+            Destroy(gameObject);
         }
     }
 }

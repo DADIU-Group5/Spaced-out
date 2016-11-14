dc4fd791021076b76676b818336cedd0c3182c8f
Bjørn <email@email.com>
2016-11-14 23:05:02 +0100

    Added progress manager logic. Not fully done yet.

--- Assets/Scripts/Data/Progress.cs
+++ Assets/Scripts/Data/Progress.cs
@@ -1,16 +1,47 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Progress", menuName = "Progress", order = 1)]
 public class Progress : ScriptableObject
 {
-    public int currency = 0;
-    public LevelData[] levelProgress;
+    public DateTime levelGenerationTime;
+    public int currency;
+    public bool completedTutorial;
+    public LevelData[] levels;
 
+    void Reset()
+    {
+        levelGenerationTime = DateTime.Now;
+        currency = 0;
+        completedTutorial = true;
+
+        SetSeeds(new int[] { 0, 1, 2, 3, 4 });
+    }
+
+    // store new seeds for levels and reset progress
+    public void SetSeeds(int[] seeds)
+    {
+        levelGenerationTime = DateTime.Now;
+        levels = new LevelData[seeds.Length];
+        for (int i = 0; i < seeds.Length; i++)
+        {
+            levels[i] = new LevelData()
+            {
+                seed = seeds[i]
+            };
+        }
+        levels[0].unlocked = true;
+    }
+
+    [Serializable]
     public struct LevelData
     {
-        public bool completedLevel;
-        public bool collectedComics;
+        public int seed;
+        public bool unlocked;
+        public bool completed;
+        public bool allComics;
         public bool noDeaths;
+
     }
 }
---
+++ Assets/Scripts/Managers/ProgressManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgressManager : Singleton<ProgressManager> {
+
+    public const int medalCompleted = 0;
+    public const int medalAllComics = 1;
+    public const int medalNoDeaths = 2;
+
+    public Progress progress;
+
+    // set medal as completed for level
+    public void SetMedal(int level, int medal)
+    {
+        if (level < 1 || level > progress.levels.Length)
+        {
+            throw new UnityException("No medals exists for level: " + level);
+        }
+
+        switch(medal)
+        {
+            case medalCompleted:
+                if (!progress.levels[level].completed) // check if completed already
+                {
+                    progress.levels[level].completed = true;
+                    progress.currency++;
+                    // unlock next level
+                    if (level < progress.levels.Length - 1)
+                        progress.levels[level + 1].unlocked = true;
+                }
+                break;
+            case medalAllComics:
+                if (!progress.levels[level].allComics) // check if completed already
+                {
+                    progress.levels[level].allComics = true;
+                    progress.currency++;
+                }
+                break;
+            case medalNoDeaths:
+                if (!progress.levels[level].noDeaths) // check if completed already
+                {
+                    progress.levels[level].noDeaths = true;
+                    progress.currency++;
+                }
+                break;
+            default:
+                throw new UnityException("Medal value not in range [0..2]: " + medal);
+        }
+    }
+
+    // set new seeds for levels
+    public void SetNewLevelSeeds(int[] seeds)
+    {
+        // check for correct number of seeds
+        if (seeds.Length != progress.levels.Length)
+        {
+            throw new UnityException("Number of seeds doesn't match number of levels");
+        }
+
+        progress.SetSeeds(seeds);
+    }
+
+    // returns an array of 3 bools indicating if medals are completed
+    public bool[] GetMedals(int level)
+    {
+        if (level < 1 || level > progress.levels.Length)
+        {
+            throw new UnityException("No medals exists for level: " + level);
+        }
+
+        var levelProgress = progress.levels[level];
+        return new bool[] { levelProgress.completed, levelProgress.allComics, levelProgress.noDeaths }; 
+    }
+}

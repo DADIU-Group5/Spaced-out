a7c3dde97fa19b273a60872727aa2395c0bb32a8
Camilla <camilla.g.jakobsen@hotmail.com>
2016-11-14 11:01:28 +0100

    LevelProgress setup

---
+++ Assets/Scripts/Data/LevelProgress.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelProgress : MonoBehaviour
+{
+    public bool tutorialComplete = false;
+    public int levelsComplete = 0;
+    public Button[] levels;
+
+	void Start ()
+    {
+	
+	}
+
+    public void ResetLevelProgress()
+    {
+        if (tutorialComplete = false)
+        {
+            //disable all levels
+        }
+        else
+        {
+            //disable all but level 1
+        }
+
+    }
+
+    public void UnlockNewLevel(int levelCompleted)
+    {
+        //bla
+    }
+
+    public void TutorialComplete()
+    {
+        tutorialComplete = true;
+    }
+}

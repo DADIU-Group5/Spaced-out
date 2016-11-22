dceac1f1834fc7fb9f3fb2f9b004a08065b382d3
Giorgos <gkritsot@gmail.com>
2016-11-22 17:06:35 +0100

    Added GAL animation script.

---
+++ Assets/GALAnimation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GALAnimation : MonoBehaviour, Observer
+{
+    private Animator animator;
+
+    // Use this for initialization
+    void Start ()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnNotify(GameObject entity, ObserverEvent evt)
+    {
+    }
+}

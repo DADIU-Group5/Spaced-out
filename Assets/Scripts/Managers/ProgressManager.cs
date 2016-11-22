a219466f53f0afa4f90ab3daca47385c55a1291b
Frederik <fknudsen@gmail.com>
2016-11-22 19:30:30 +0100

    Changed UI things in tutorial Removed several debug.logs and prints.

--- Assets/Scripts/Managers/ProgressManager.cs
+++ Assets/Scripts/Managers/ProgressManager.cs
@@ -68,7 +68,6 @@ public class ProgressManager : Singleton<ProgressManager> {
     // returns an array of 3 bools indicating if medals are completed
     public bool[] GetMedals(int level)
     {
-        Debug.Log("Getting medals!");
         if (level < 0 || level > progress.levels.Length)
         {
             throw new UnityException("No medals exists for level: " + level);

6d782c9a2a41fec17c28b7bf6326f5d04101bc7e
Frederik <fknudsen@gmail.com>
2016-11-22 19:21:45 +0100

    Shows fps ingame.

--- Assets/Scripts/DebugSeedText.cs
+++ Assets/Scripts/DebugSeedText.cs
@@ -5,11 +5,17 @@ using System.Collections;
 public class DebugSeedText : MonoBehaviour {
 
     public Text seedText;
+    GenerationData.LevelData data;
 
 	// Use this for initialization
 	void Start () {
-        var data = GenerationDataManager.instance.GetLevelData();
+        data = GenerationDataManager.instance.GetLevelData();
         seedText.text = "Exterior seed: " + data.exteriorSeed + "\nInterior seed: " + data.interiorSeed;
+    }
+
+    void Update()
+    {
+        seedText.text = "Exterior seed: " + data.exteriorSeed + "\nInterior seed: " + data.interiorSeed+"\nFPS: "+(int)(1f/Time.deltaTime);
     }
 	
 }

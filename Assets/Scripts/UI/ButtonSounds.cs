c87aeeb0a898bcc64fb9ac31885a54c9fe5af42b
Mia <mia@sogaard.us>
2016-11-24 14:56:47 +0100

    Added sounds to menu buttons.

---
+++ Assets/Scripts/UI/ButtonSounds.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonSounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MenuClickForwards()
+    {
+        var evt = new ObserverEvent(EventName.ToggleUI);
+        evt.payload.Add(PayloadConstants.TYPE, SoundEventConstants.MENU_CLICK_FORWARDS);
+        Subject.instance.Notify(gameObject, evt);
+    }
+
+    public void MenuClickBackwards()
+    {
+        var evt = new ObserverEvent(EventName.ToggleUI);
+        evt.payload.Add(PayloadConstants.TYPE, SoundEventConstants.MENU_CLICK_BACKWARDS);
+        Subject.instance.Notify(gameObject, evt);
+    }
+
+    public void MenuPressStart()
+    {
+        var evt = new ObserverEvent(EventName.ToggleUI);
+        evt.payload.Add(PayloadConstants.TYPE, SoundEventConstants.MENU_PRESS_START);
+        Subject.instance.Notify(gameObject, evt);
+    }
+
+    public void MenuScroll()
+    {
+        var evt = new ObserverEvent(EventName.ToggleUI);
+        evt.payload.Add(PayloadConstants.TYPE, SoundEventConstants.MENU_SCROLL);
+        Subject.instance.Notify(gameObject, evt);
+    }
+}

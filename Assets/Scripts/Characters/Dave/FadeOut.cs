2ce961e8740a5f1b84692e02dcd38a99f5bd2412
Mia <mia@sogaard.us>
2016-11-25 14:21:55 +0100

    Updated switchItem to change colour.

---
+++ Assets/Scripts/Characters/Dave/FadeOut.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeOut : MonoBehaviour {
+
+    public float m_Transparency = 0.5f;
+    /*public Shader m_OldShader = renderer.material.shader;
+    public Color m_OldColor  = renderer.material.color;*/
+
+    // Use this for initialization
+    void Update () {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<Renderer>() != null)
+            {
+                transform.GetChild(i).GetComponent<Renderer>().material.shader = Shader.Find("Transparent/Diffuse");
+                Color C = transform.GetChild(i).GetComponent<Renderer>().material.color;
+               // C.albe
+                C.a = m_Transparency;
+                transform.GetChild(i).GetComponent<Renderer>().material.color = C;
+            }
+        }
+    }
+	
+}

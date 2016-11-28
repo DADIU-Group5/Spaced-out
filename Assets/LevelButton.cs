d46ab2b0b89555b5fbae378c363c5321f095f2d2
Bjørn <gkritsot@gmail.com>
2016-11-28 17:27:28 +0100

    tmp checkpoint

---
+++ Assets/LevelButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class LevelButton : MonoBehaviour {
+
+    private Image image;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public IEnumerator FadeIn()
+    {
+        StopAllCoroutines();
+        float a = image.color.a;
+
+        while()
+    }
+
+    public void FadeOut()
+    {
+        print("Out");
+    }
+}

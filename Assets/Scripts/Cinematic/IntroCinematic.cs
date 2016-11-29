df03b1e4c923de566a672663a0061456d0d61309
Bjørn <gkritsot@gmail.com>
2016-11-29 18:27:29 +0100

    cutscene is ready for music

--- Assets/Scripts/Cinematic/IntroCinematic.cs
+++ Assets/Scripts/Cinematic/IntroCinematic.cs
@@ -4,11 +4,33 @@ using UnityEngine.SceneManagement;
 
 public class IntroCinematic : MonoBehaviour {
 
+    public Animator animator;
+    public Animator galAnimator;
+
 	// Use this for initialization
 	void Start () {
         GetComponent<SimpelAnimation>().PlayAnimations(() =>
         {
-            //SceneManager.LoadScene("TutStage01");
+            SceneManager.LoadScene("TutStage01");
         });
+        Invoke("PlayRest", 10f);
+        Invoke("HappyTalk", 5f);
+        Invoke("SadTalk", 35f);
 	}
+
+    private void PlayRest()
+    {
+        animator.SetTrigger("Rest");
+    }
+
+    private void HappyTalk()
+    {
+        galAnimator.SetTrigger("happyTalk");
+    }
+
+    private void SadTalk()
+    {
+        galAnimator.SetTrigger("sadTalk");
+        Invoke("HappyTalk", 0.5f);
+    }
 }

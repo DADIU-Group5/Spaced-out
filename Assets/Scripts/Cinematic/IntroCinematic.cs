8094442915639d76d21246d5acb21d16916cc593
Bjørn <gkritsot@gmail.com>
2016-11-29 15:29:10 +0100

    intro cinematic

---
+++ Assets/Scripts/Cinematic/IntroCinematic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class IntroCinematic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SimpelAnimation>().PlayAnimations(() =>
+        {
+            //SceneManager.LoadScene("TutStage01");
+        });
+	}
+}

32dc24c2bd3603f568e1ebae5fbd6be03f0550b5
Malte <lel@ecksdee.retardiano>
2016-12-05 16:02:50 +0100

    Escape pod sign now changes language!

---
+++ Assets/Scripts/Tutorial/EscapePodSign.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EscapePodSign : MonoBehaviour, Observer {
+
+    public Material matDanish, matEnglish;
+
+    private Renderer renderer;
+
+    void Start () {
+        renderer = GetComponent<Renderer>();
+        SetLanguage();
+    }
+
+    private void SetLanguage()
+    {
+        switch (SettingsManager.instance.GetLanguage())
+        {
+            case Language.Danish:
+                renderer.material = matDanish;
+                break;
+            case Language.English:
+                renderer.material = matEnglish;
+                break;
+        }
+    }
+
+    public void OnNotify(GameObject entity, ObserverEvent evt)
+    {
+        if (evt.eventName == EventName.ChangeLanguage)
+        {
+            SetLanguage();
+        }
+    }
+
+    public void OnDestroy()
+    {
+        Subject.instance.RemoveObserver(this);
+    }
+}

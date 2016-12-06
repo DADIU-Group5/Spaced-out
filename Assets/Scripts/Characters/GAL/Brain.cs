f973c1fcd7cabb2f7bdf98ca6c90a2716ee11075
Giorgos <gkritsot@gmail.com>
2016-12-06 13:56:08 +0100

    Made it so that GAL makes the same comment when you die from fire and explosions. If we have time, we must add subtitles for explosions and change to SubtitleType.Explosion

--- Assets/Scripts/Characters/GAL/Brain.cs
+++ Assets/Scripts/Characters/GAL/Brain.cs
@@ -97,7 +97,7 @@ public class Brain : Singleton<Brain>, Observer
                     type = SubtitleType.OutOfOxygen;
                     break;
                 case EventName.PlayerExploded:
-                    type = SubtitleType.Explosion;
+                    type = SubtitleType.GasLeak;
                     break;
             }
         }

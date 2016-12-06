f54cf74b87b70ce9c199fbdf20b145ce58284728
Giorgos <gkritsot@gmail.com>
2016-12-06 16:00:46 +0100

    Maintaining previous rtpc value so that the charge plays different sounds. Door sound tweaks, unsuccessful

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -15,6 +15,9 @@ public class SoundManager : Singleton<SoundManager>, Observer
     private bool musicPlaying;
     private bool atmoshericSoundPlaying;
 
+    private float newRTPCForCharge;
+    private float previousRTPCFC;
+
     // Use this for initialization
     void Start()
     {
@@ -51,9 +54,7 @@ public class SoundManager : Singleton<SoundManager>, Observer
 
             case EventName.PlayerLaunch:
                 PlayEvent(SoundEventConstants.DAVE_LAUNCH, gameObject);
-
                 chargePlaying = false;
-
                 break;
 
             case EventName.OnFire:
@@ -84,7 +85,7 @@ public class SoundManager : Singleton<SoundManager>, Observer
                         break;
 
                     case JetPackState.StopCharging:
-                        AkSoundEngine.SetRTPCValue("jetpackChargeLevel", 0);
+                        //AkSoundEngine.SetRTPCValue("jetpackChargeLevel", 0);
                         StopEvent(SoundEventConstants.DAVE_CHARGE, 0.0f);
                         chargePlaying = false;
 
@@ -93,8 +94,12 @@ public class SoundManager : Singleton<SoundManager>, Observer
                 break;
 
             case EventName.LaunchPowerChanged:
+                previousRTPCFC = newRTPCForCharge;
+                
                 var forceVec = (Vector2)evt.payload[PayloadConstants.LAUNCH_FORCE];
-                AkSoundEngine.SetRTPCValue("jetpackChargeLevel", forceVec.x * 10.0f);
+                newRTPCForCharge = forceVec.x * 10.0f;
+
+                AkSoundEngine.SetRTPCValue("jetpackChargeLevel", previousRTPCFC);
                 break;
 
             case EventName.Collision:

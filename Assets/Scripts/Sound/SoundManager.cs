4dce3664a8e59825de684468f9e9e57b3d65a87b
Giorgos <gkritsot@gmail.com>
2016-12-06 19:04:55 +0100

    added new rtpc with bjørn. its constantly sending player velocity.

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -47,14 +47,22 @@ public class SoundManager : Singleton<SoundManager>, Observer
     {
         switch (evt.eventName)
         {
+            case EventName.PlayerLaunch:
+                PlayEvent(SoundEventConstants.DAVE_LAUNCH, gameObject);
+                chargePlaying = false;
+                break;
+
             case EventName.Narrate:
                 var eventName = (string)evt.payload[PayloadConstants.NARRATIVE_ID];
                 PlayEvent(eventName);
                 break;
 
-            case EventName.PlayerLaunch:
-                PlayEvent(SoundEventConstants.DAVE_LAUNCH, gameObject);
-                chargePlaying = false;
+            case EventName.PlayerVelocity:
+                var velocity = (float)evt.payload[PayloadConstants.VELOCITY];
+               // print("Victor velocity: " + velocity);
+                AkSoundEngine.SetRTPCValue("velocity2", velocity * 10);  
+                // VICOYR
+
                 break;
 
             case EventName.OnFire:

efb0a902422700ac8c1b617ac414696e69f4b6d7
Bjørn <gkritsot@gmail.com>
2016-12-08 09:55:28 +0100

    Fixed comic hovering

--- Assets/Scripts/CollectibleRotator.cs
+++ Assets/Scripts/CollectibleRotator.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class CollectibleRotator : MonoBehaviour {
 
-    public float rotationSpeed;
+    public float rotationSpeed, hoverScale = 5f;
     public AnimationCurve vertical;
+    public Vector3 startPos;
+
+    void Awake()
+    {
+        startPos = transform.position;
+    }
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
         transform.Rotate(Vector3.up, rotationSpeed);
-        transform.Translate(Vector3.up * vertical.Evaluate(Time.time));
+        //transform.Translate(Vector3.up * vertical.Evaluate(Time.time));
+        transform.position = startPos + Vector3.up * vertical.Evaluate(Time.time) * hoverScale;
 	}
 }
